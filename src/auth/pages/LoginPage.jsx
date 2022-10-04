e7e61f5cc44f5ba547745743a07ef90fefc6c513
danielmamian99 <danimaster99@gmail.com>
2022-10-03 23:37:41 -0500

    animations effects

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,34 +1,48 @@
 import { Link as RouterLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
-import { Alert, Button, Grid, Link, TextField, Typography } from "@mui/material";
+import {
+  Alert,
+  Button,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { Google } from "@mui/icons-material";
 
 import { AuthLayout } from "../layout/AuthLayout";
 import { useForm } from "../../hooks";
-import { chekingAuthentication, startGoogleSingIn, startLoginWithEmailPassword } from "../../store/auth";
+import {
+  chekingAuthentication,
+  startGoogleSingIn,
+  startLoginWithEmailPassword,
+} from "../../store/auth";
 import { useMemo } from "react";
 
 export const LoginPage = () => {
-  const { status, errorMessage }  = useSelector(state => state.auth)
-  const dispatch = useDispatch()
+  const { status, errorMessage } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
   const { email, password, onInputChange } = useForm({
     email: "",
     password: "",
   });
 
-  const isAuthenticating = useMemo(() => status === 'cheking', [status])
+  const isAuthenticating = useMemo(() => status === "cheking", [status]);
 
   const onSubmit = (event) => {
     event.preventDefault();
-    dispatch(startLoginWithEmailPassword({email, password}))
+    dispatch(startLoginWithEmailPassword({ email, password }));
   };
-  const onGoogleSingIn = ()=>{
-    dispatch(startGoogleSingIn())
-  }
+  const onGoogleSingIn = () => {
+    dispatch(startGoogleSingIn());
+  };
   return (
     <AuthLayout title="Login">
-      <form onSubmit={onSubmit}>
+      <form
+        className="animate__animated animate__fadeIn animate__faster"
+        onSubmit={onSubmit}
+      >
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -53,22 +67,32 @@ export const LoginPage = () => {
               onChange={onInputChange}
             />
           </Grid>
-          <Grid item xs={12} display={!!errorMessage ? "" : "none"} sx={{mt: 1 }}>
+          <Grid
+            item
+            xs={12}
+            display={!!errorMessage ? "" : "none"}
+            sx={{ mt: 1 }}
+          >
             <Alert severity="error">{errorMessage}</Alert>
           </Grid>
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12} sm={6}>
-              <Button type="submit" variant="contained" 
-              disabled = {isAuthenticating}
-              fullWidth>
+              <Button
+                type="submit"
+                variant="contained"
+                disabled={isAuthenticating}
+                fullWidth
+              >
                 Login
               </Button>
             </Grid>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" 
-              disabled = {isAuthenticating}
-              fullWidth
-              onClick={onGoogleSingIn}>
+              <Button
+                variant="contained"
+                disabled={isAuthenticating}
+                fullWidth
+                onClick={onGoogleSingIn}
+              >
                 <Google />
                 <Typography sx={{ ml: 1 }}>Google</Typography>
               </Button>
