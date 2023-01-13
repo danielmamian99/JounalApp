75d31aa339f963232a286aedb6e19a16d14e4b31
danielmamian99 <danimaster99@gmail.com>
2023-01-13 16:59:57 -0500

    image implemented

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -4,16 +4,19 @@ import {
   Box,
   Divider,
   Drawer,
+  Grid,
   List,
   Toolbar,
   Typography,
 } from "@mui/material";
 
 import { SideBarItem } from "./SideBarItem";
+import Image from 'mui-image'
 
 export const SideBar = ({ drawerWidth = 240 }) => {
   const { displayName, photoURL } = useSelector((state) => state.auth);
   const { notes } = useSelector((state) => state.journal);
+  console.log('photoURL >>>', photoURL);
   return (
     <Box
       component="nav"
@@ -31,6 +34,11 @@ export const SideBar = ({ drawerWidth = 240 }) => {
           <Typography variant="h6" noWrap component="div">
             {displayName}
           </Typography>
+          {photoURL && 
+          <Grid sx={{display:'flex', justifyContent:'center'}}>
+            <Image src={photoURL} fit="cover" height='80%' width='50%' showLoading={true}/>
+          </Grid>
+          }
         </Toolbar>
         <Divider />
 
